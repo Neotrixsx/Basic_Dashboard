8d58932f8fe909c401db9465d8ec0dc376a96fac
Suraj Gorai <gorai.suraj@gmail.com>
2018-03-09 18:13:51 +0530

    SQL Added

---
+++ SQL(Database)/impdatabase.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 09, 2018 at 01:43 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 5.6.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `impdatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admininfo`
+--
+
+CREATE TABLE `admininfo` (
+  `adminid` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `link` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admininfo`
+--
+
+INSERT INTO `admininfo` (`adminid`, `username`, `password`, `link`) VALUES
+(1, 'admin@admin.com', '123qwe', 'https://ionicframework.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `catid` int(11) NOT NULL,
+  `category` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userinfo`
+--
+
+CREATE TABLE `userinfo` (
+  `userid` int(11) NOT NULL,
+  `canpany_name` varchar(100) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `website` varchar(100) NOT NULL,
+  `contact_person` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userinfo`
+--
+
+INSERT INTO `userinfo` (`userid`, `canpany_name`, `address`, `website`, `contact_person`, `email`, `phone`) VALUES
+(1, 'Dummy ', 'Dummy Address', 'https://www.dummy.com/', 'Dummy Person', 'dummy@dummy.com', '123456789'),
+(2, 'Test', 'Test Test', 'https://www.w3schools.com/', 'Test', 'aest@aa.aa', '123456877'),
+(3, 'something', 'something something', 'https://ionicframework.com', 'something ', 'something@something.com', '987654321');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admininfo`
+--
+ALTER TABLE `admininfo`
+  ADD PRIMARY KEY (`adminid`);
+
+--
+-- Indexes for table `userinfo`
+--
+ALTER TABLE `userinfo`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admininfo`
+--
+ALTER TABLE `admininfo`
+  MODIFY `adminid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `userinfo`
+--
+ALTER TABLE `userinfo`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
