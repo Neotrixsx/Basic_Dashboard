020789c8efdd965cdc6a487ed2eec365e9f97dda
Suraj Gorai <gorai.suraj@gmail.com>
2018-03-13 18:40:17 +0530

    -

--- pages/includephp/deletecat.php
+++ pages/includephp/deletecat.php
@@ -4,6 +4,7 @@
         $update = "DELETE FROM `category` WHERE `category`.`catid` = $php_var";
         if ($db->query($update) === TRUE) {
             echo "<script type='text/javascript'> successtoastdelcat(); </script>";
+            echo "<script type='text/javascript'> setTimeout(function(){   window.location.reload(); }, 500); </script>";
         } else {
             echo "Error updating record: " . $db->error;
             echo "<script type='text/javascript'> warningtoast(); </script>";
